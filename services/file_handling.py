e8dda3ce6c6afd811aa95265c3e84f18874071b2
ecocoo <sudoplatov.05@mail.ru>
2024-11-19 00:24:28 +0500

    рабочая версия

---
+++ services/file_handling.py
@@ -0,0 +1,41 @@
+import os
+import sys
+BOOK_PATH = 'book/Witcher_part_1.txt'
+PAGE_SIZE = 1050
+
+book = {}
+
+
+def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
+    end_signs = ',.!:;?'
+    counter = 0
+    if len(text) < start + size:
+        size = len(text) - start
+        text = text[start:start + size]
+    else:
+        if text[start + size] == '.' and text[start + size - 1] in end_signs:
+            text = text[start:start + size - 2]
+            size -= 2
+        else:
+            text = text[start:start + size]
+        for i in range(size - 1, 0, -1):
+            if text[i] in end_signs:
+                break
+            counter = size - i
+    page_text = text[:size - counter]
+    page_size = size - counter
+    return page_text, page_size
+
+
+def prepare_book(path):
+    with open(path, 'r', encoding='utf-8') as file:
+        text = file.read()
+    start, page_number = 0, 1
+    while start < len(text):
+        page_text, page_size = _get_part_text(text, start, PAGE_SIZE)
+        book[page_number] = page_text
+        start += page_size
+        page_number += 1
+
+
+prepare_book(BOOK_PATH)
